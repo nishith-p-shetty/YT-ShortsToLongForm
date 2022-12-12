d4caaa10dfd3c5477aaa770f7fa65cbe8e52db09
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-12-13 00:24:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import os
 
-path = "/Users/nishithshetty/Downloads/vid/"
-title1 = "Reddit Compilation #"
-title2 = " | Reddit shorts | Redditcasm.mp4"
+path = input("Enter the path of the folder containing shorts : ")
 
 def main():
         count = 1
-        for i in range(1,11):                          # no of folder
-            with open("list.txt", 'w') as file:
-                cur_path = path + str(i)                   # path + current folder name
-                video_list = os.listdir(cur_path)          #list of files names
-                if ".DS_Store" in video_list : video_list.remove(".DS_Store")
-                for x in video_list:                        # iterate over al file names
-                    os.rename( (cur_path + "/" + x), (cur_path + "/" + str(count) + ".mp4"))
-                    file.write("file " + cur_path + "/" + str(count) + ".mp4\n")
+        with open( (path + "/list.txt"), 'w') as file:
+            shorts_list = os.listdir(path)          #list of files names
+            for short in shorts_list:               # iterate over al file names
+                if (short.endswith('.mp4')):
+                    os.rename( (path + "/" + short), (path + "/" + str(count) + ".mp4"))
+                    file.write("file " + path + "/" + str(count) + ".mp4\n")
                     count+=1
-                count=1
-            #ffmpeg -f concat -safe 0 -i list.txt -c copy /Users/nishithshetty/Downloads/vid/result/output.mp4
-            os.system("ffmpeg -f concat -safe 0 -i list.txt -c copy /Users/nishithshetty/Downloads/vid/result/" + str(i) + ".mp4 -y")
-
+        os.system("ffmpeg -f concat -safe 0 -i \"" + path + "/list.txt\" -c copy \"" + path + "/output.mp4\" -y")
+        os.remove(path + "/list.txt")
+        
 if __name__ == "__main__":
     main();
